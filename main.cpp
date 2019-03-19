be296f7754df2556157e69ff2e4469bb42c78ea1
Alice(ParshinAD) <u211-18@public.mpei.local>
2019-03-19 15:25:47 +0300

    code:double

--- main.cpp
+++ main.cpp
@@ -5,18 +5,13 @@ using namespace std;
 int main()
 {
     cout << "enter A and B: " << endl;
-    int a,b;
+    double a,b;
     cin >> a >> b;
     cout << "A + B = " << a + b << endl
          << "A - B = " << a - b << endl
          << "A * B = " << a * b << endl
          << "A / B = " << a / b << endl;
-<<<<<<< HEAD
          if (a>b) cout << "MAX = " << a;
          else cout << "MAX = " << b;
-=======
-         if (a>b) cout << "MIN = " << b;
-         else cout << "MIN = " << a;
->>>>>>> code: боб с минимумом
     return 0;
 }
